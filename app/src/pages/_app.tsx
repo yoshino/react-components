623a5477b67f2f7111a2120e4ee811aec0361a2b
yoshino <44nocycle@gmail.com>
2023-01-31 21:30:12 +0900

    Fix axe violations

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useAxe } from '../hooks/useAxe'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   useAxe()
 
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>React Components</title>
+      </Head>
+
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  )
 }
