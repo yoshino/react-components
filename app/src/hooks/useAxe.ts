87e34e7853e5fe1b9272730762aa0cc31f822864
yoshino <44nocycle@gmail.com>
2023-01-31 21:08:50 +0900

    Setup axe

---
+++ app/src/hooks/useAxe.ts
@@ -0,0 +1,16 @@
+import React, { useCallback, useEffect } from 'react'
+
+export const useAxe = () => {
+  useEffect(() => {
+    if (process.env.NODE_ENV !== 'development') {
+      return
+    }
+    ;(async () => {
+      const axe = await import('@axe-core/react')
+      const locale = await import('axe-core/locales/ja.json')
+      const ReactDOM = await import('react-dom')
+      // @ts-expect-error For the error of package type
+      axe.default(React, ReactDOM, 1000, { locale })
+    })()
+  }, [])
+}
--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useAxe } from '../hooks/useAxe'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useAxe()
+
   return <Component {...pageProps} />
 }
