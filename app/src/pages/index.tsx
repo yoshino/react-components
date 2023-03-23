813c992712bf9d8fc86afa501661ffdf10787ecc
yoshino <44nocycle@gmail.com>
2023-03-23 14:24:55 +0900

    cosmetic

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className='text-3xl mb-4'>Hi!</h1>
+    <div className='m-10'>
+      <h1 className='text-3xl mb-4'>Links</h1>
       <Link href='/hacker-news' legacyBehavior>
-        <a className=' bg-blue-300 text-white py-1 px-3 rounded-md'>HackerNews(API Fetch)</a>
+        <a className='text-xl text-blue-300 font-bold my-4'>HackerNews(API Fetch)</a>
       </Link>
     </div>
   )
